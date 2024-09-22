a8e372c606cf6e5d809da46c01ea0372237c824d
unknown <marianabarros.fb@gmail.com>
2024-09-22 17:46:10 -0400

    Revisao de contrato att

--- beecrowd-1120.cpp
+++ beecrowd-1120.cpp
@@ -2,35 +2,6 @@
 
 using namespace std;
 
-// int main()
-// {
-//     string c, d;
-//     cin >> c >> d;
-
-//     while (c != "0" && d != "0")
-//     {
-//         string res;
-
-//         for (auto n : d)
-//         {
-//             if (n != c[0])
-//             {
-//                 res.push_back(n);
-//             }
-//         }
-
-//         if (res == "" || res[0] == '0')
-//         {
-//             res = "0";
-//         }
-
-//         cout << res << endl;
-//         cin >> c >> d;
-//     }
-
-//     return 0;
-// }
-
 int main()
 {
     string n, d;
